1f1f63d8416f3ecf885b85a31e7fe70aac087ed9
Default user <yalejandro.gomez@gmail.com>
2019-11-18 13:47:32 +0000

    Actualizar archivos

---
+++ EulerODE.cpp
@@ -0,0 +1,40 @@
+#include <fstream>
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+const double K = 10.0;
+const double M = 2.0;
+const double DeltaT = 0.01;
+
+int Euler(double t,double v0,double x0,string nombre);
+
+int main(){
+    double t = 10.0;
+    double v0 = 0.0;
+    double x0 = 1.0;
+    string nombre = "14Euler.dat";
+    
+    Euler(t,v0,x0,nombre);
+    
+    return 0;
+}
+
+int Euler(double t,double v0,double x0,string nombre){
+    ofstream outfile;
+    outfile.open(nombre);
+    double xOld,vOld;
+    double v = v0;
+    double x = x0;
+    for(double i=0;i<=t;i+=DeltaT){
+        xOld=x;
+        vOld=v;
+        x += DeltaT*vOld;
+        v -= DeltaT*(K/M)*xOld;
+        
+        outfile << i << "\t" << x << "\t" << v << endl;
+    }
+    outfile.close();
+    
+    return 0;
+}
---
+++ EulerODE.py
@@ -0,0 +1,27 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+ODE = np.loadtxt("14Euler.dat")
+
+plt.figure(figsize=(12,4))
+
+time = ODE[:,0]
+x = ODE[:,1]
+v = ODE[:,2]
+
+plt.subplot(1,2,1)
+plt.plot(time,x,c='g')
+plt.grid()
+plt.xlabel('time(s)')
+plt.ylabel('x(m)')
+plt.title("Harmonic oscillator, Position")
+
+plt.subplot(1,2,2)
+plt.plot(time,v,c='g')
+plt.grid()
+plt.xlabel('time(s)')
+plt.ylabel('v(m/s)')
+plt.title("Harmonic oscillator, Velocity")
+
+plt.savefig("EulerODE.png")
